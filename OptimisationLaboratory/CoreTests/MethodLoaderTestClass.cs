aba7d15f5151f7d609c80c8427c1a762e76fdd7b
PetrovJurij <35653409+PetrovJurij@users.noreply.github.com>
2018-02-19 14:11:42 +0200

    [+]Test for MethodLoader if all of the instances are IOptimisationMethod

---
+++ OptimisationLaboratory/CoreTests/MethodLoaderTestClass.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using System.IO;
+using System;
+
+namespace CoreTests
+{
+    [TestClass]
+    public class MethodLoaderTestClass
+    {
+        [TestMethod]
+        public void TestLoadsAllIOptimisationMethods()
+        {
+            MethodsLoader loader = new MethodsLoader(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..")) + "\\GUI\\bin\\Debug\\methods\\");
+            var instances=loader.Load();
+            foreach (var instance in instances)
+                Assert.IsInstanceOfType(instance, typeof(IOptimisationMethod));
+        }
+    }
+}
---
+++ OptimisationLaboratory/CoreTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoreTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CoreTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("322c11c8-c164-4d3f-89d0-209e137949cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
