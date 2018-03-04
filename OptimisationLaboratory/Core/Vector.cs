745cb0d6b170b09322431c193669be28123cbf2d
PetrovJurij <35653409+PetrovJurij@users.noreply.github.com>
2018-03-04 10:05:20 +0200

    [+]Vector class

---
+++ OptimisationLaboratory/Core/Vector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class Vector
+    {
+        private double[] vec;
+        private int n;
+
+        public Vector()
+        { n = 0; }
+
+        public Vector(double []x)
+        {
+            n = x.Length;
+            vec = new double[n];
+            x.CopyTo(vec,0);
+        }
+
+        public Vector(int n)
+        {
+            this.n = n;
+
+            vec = new double[n];
+            for (int i = 0; i < n; i++)
+                vec[i] = 0;
+        }
+
+        public Vector(Vector v)
+        {
+            n = v.n;
+            v.vec.CopyTo(vec, 0);
+        }
+
+        public Vector(Matrix Mat)
+        {
+            if (Mat.M > 1 && Mat.N > 1)
+                throw new ArgumentException("Матрица должна быть " +
+                                            "либо столбцом, либо строкой.");
+        }
+
+        public double this[int index]
+        {
+            get { return vec[index]; }
+        }
+
+
+
+    }
+}
